4ba296760483ccb40b3a0ba249942eb529872b33
Devlyn11 <125839688+Devlyn11@users.noreply.github.com>
2023-02-21 10:55:01 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,33 +43,22 @@ try:
 except URLError as e:
   streamlit.error()
     
-#streamlit.write('The user entered ', fruit_choice)
+streamlit.header("The fruit load list contains:")
 
+def get_fruit_load_list():
+with my_cnx.cursor() as my_cur:
+my_cur.execute("select * from fruit_load_list")
+return my_cur.fetchall()
 
-#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-#streamlit.text(fruityvice_response.json()) #No need to print line
-
-# Normalise JSON to tabular format 
-#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# Print normalised data
-#streamlit.dataframe(fruityvice_normalized)
+#Button to load fruit
+if streamlit.button('Get Fruit List'):
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_data_rows=get_fruit_load_list()
+streamlit.dataframe(my_data_row)
 
 streamlit.stop()
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
 
-
-#my_cnx = snowflake.connector.connect (**streamlit.secrets["snowflake"])
-#my_cur = my_cnx.cursor()
-my_cur.execute ("select * from fruit_load_list")
-my_data_row=my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_row)
 
 fruit_add = streamlit.text_input('What fruit would you like to add?','Jackfruit')
 streamlit.write('Thanks for adding ', fruit_add)
